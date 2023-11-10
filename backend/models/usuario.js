41fb81e54d7190fdb8e119eab688f682f2d411e1
Guillermo <gbustamante21@alumnos.utalca.cl>
2023-11-09 23:43:45 -0300

    Creación de frontend y backend base.

---
+++ backend/models/usuario.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const usuarioSchema = new Schema({
+    nombre: String,
+    correo: {type:String,unique:true},
+    contrasena:String
+},{ versionKey: false }
+);
+
+const usuario = mongoose.model('usuario', usuarioSchema);
+
+export default usuario;
---
+++ backend/routes/usuario.js
@@ -0,0 +1,92 @@
+import express from 'express';
+const router = express.Router();
+
+// importar el modelo nota
+import usuarioSchema from '../models/usuario';
+
+
+
+router.post('/addUsuario', async(req, res) => {
+  const body = req.body;  
+ 
+  const usuario = usuarioSchema(body)       
+  await usuario.save()
+  .then((result) => {
+    res.json(result)
+    
+  })
+  .catch((err) => {
+    console.log(err)
+    res.json(err)
+  }); 
+  }
+);
+
+
+router.get('/getUsuarioByID/:id', async(req, res) => {
+  const id = req.params.id;  
+ 
+  const filtro = {"_id":id}
+      
+  await usuarioSchema.findById(filtro)
+  .then((result) => {
+    res.json(result)
+    
+  })
+  .catch((err) => {
+    console.log(err)
+    res.json(err)
+  }); 
+  }
+);
+
+router.post('/getAllUsuarios', async(req, res) => {
+
+  await usuarioSchema.find()
+  .then((result) => {
+    res.json(result)
+    
+  })
+  .catch((err) => {
+    console.log(err)
+    res.json(err)
+  }); 
+  }
+);
+router.put('/updateUsuarioByNombre', async(req, res) => {
+  const id = req.body.id;  
+  const nombre = req.body.nombre;
+ 
+  
+  const filtro = {"_id":id}
+
+  const cambio = {"nombre":nombre}
+  //{new: true} retorna el documento modificado
+  await usuarioSchema.findOneAndUpdate(filtro, cambio, {new: true})
+  .then((result) => {
+    res.json(result)
+    
+  })
+  .catch((err) => {
+    console.log(err)
+    res.json(err)
+  }); 
+  }
+);
+router.delete('/deleteUsuarioByID', async(req, res) => {
+  const id = req.body.id;  
+  
+  const filtro = {"_id":id}
+  await usuarioSchema.deleteOne(filtro)
+  .then((result) => {
+    res.json(result)
+    
+  })
+  .catch((err) => {
+    console.log(err)
+    res.json(err)
+  }); 
+  }
+);
+
+module.exports = router;
