d43b65136437820c23b3d251074383e5e05b0444
AllanVallejos <103338151+AllanVallejos@users.noreply.github.com>
2023-11-11 19:30:04 -0300

    integracion modal boton login

--- src/frontend/src/components/index/indexHeader.vue
+++ src/frontend/src/components/index/indexHeader.vue
@@ -2,12 +2,23 @@
 <!-- Desde aqui se accede al login mediante el componente indexModalLogin-->
 <script setup>
 import indexModalLogin from './indexModalLogin.vue';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+import { ref } from "vue";
+
+const visible = ref(false);
 
 </script>
 
 <template>
     <div>
-        <h1>Aqui va el contenido del Header del landing page</h1>
-        <indexModalLogin></indexModalLogin>
-    </div>
-</template>
+        <Button label="Show" icon="pi pi-external-link" @click="visible = true" />
+
+        <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50rem' }"
+            :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
+            <indexModalLogin></indexModalLogin>
+        </Dialog>
+
+    <h1>Aqui va el contenido del Header del landing page</h1>
+    
+</div></template>
