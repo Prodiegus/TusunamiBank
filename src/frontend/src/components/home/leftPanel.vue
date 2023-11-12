76779d214bdddb8ee5993614da991ff91642d647
VCornejo10 <vcornejo19@alumnos.utalca.cl>
2023-11-12 18:24:59 -0300

    Integración de botones de left panel (solo vista)
    Integracion de botones
    "Inicio", "Cuenta", "Retiros", "Depositos", "Comisiones"
    Solo visual

--- src/frontend/src/components/home/leftPanel.vue
+++ src/frontend/src/components/home/leftPanel.vue
@@ -1,10 +1,46 @@
 <script setup>
-
+    import Button from 'primevue/button';
 </script>
 
 <template>
-    <div>
-        <h1>Aqui va el contenido del panel izquierdo de Home</h1>
+    <body>
+        <div class="contenedor">
+        <div class="boton">
+            <img src="../icons/pitufo.jpg" class="icono">
+            <Button label="Inicio" link />
+        </div>
+        
+        <div class="boton">
+            <img src="../icons/Cuentas.jpg" class="icono">
+            <Button label="Cuenta" link />
+        </div>
+        
+        <div class="boton">
+            <img src="../icons/bob.jpg" class="icono">
+            <Button label="Retiros" link />
+        </div>
+        
+        <div class="boton">
+            <img src="../icons/Deposito.jpg" class="icono">
+            <Button label="Depositos" link />
+        </div>
 
+        <div class="boton">
+            <img src="../icons/Comision.jpg" class="icono">
+            <Button label="Comisiones" link />
+        </div>
+        
     </div>
+    </body>
+    
 </template>
+
+<style>
+.body {
+    background-color: #D52B1E;
+}
+.icono{
+    width: 75px;
+    height: 75px;
+}
+</style>
