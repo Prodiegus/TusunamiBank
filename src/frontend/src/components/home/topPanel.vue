75f0fc17c7de21a5bc0301ec48e756a923d261dd
NicolasIbarraMoya <80929969+NicolasIbarraMoya@users.noreply.github.com>
2023-11-13 15:21:51 -0300

    Avance top panel
    Se crea el componente top panel, el cual contiene el logo (temporal de muestra) de la página, nombre del banco, etc. Este incluye botones pero no son funcionales de momento

--- src/frontend/src/components/home/topPanel.vue
+++ src/frontend/src/components/home/topPanel.vue
@@ -1,10 +1,106 @@
 <script setup>
-
+    import Button from 'primevue/button';
 </script>
 
 <template>
     <div>
-        <h1>Aqui va el contenido del panel superior de Home</h1>
-
+      <div class="top-panel">
+            <div class="logo-container">
+                <img src="../icons/logo.png" class="icono">
+                <div class="text-container">
+                    <p class="logo-text">Tusunami</p>
+                    <p class="subtitle">"No lo vimos venir"</p>
+                </div>
+                <img src="../icons/estrella.png" class="small-icono">
+            </div>
+            <div class="spacer"></div>
+            <div class="button-container">
+                <Button class="boton" label="Sucursales" :ripple="false"/>
+                <div class="vertical-line"></div>
+                <p style="margin-right: 20px;">Hola, Sebastián Piñera</p>
+                <img src="../icons/choripan.jpg" class="rounded-icono">
+                <Button class="boton" label="Cerrar" :ripple="false"/>
+            </div>
+      </div>
     </div>
 </template>
+
+<style scoped>
+    .top-panel {
+        background-color: #0039A6;
+        height: 100%;
+        width: 100%;
+        align-items: left;
+        display: flex;
+        position: relative;
+    }
+
+    .logo-container {
+        display: flex;
+        align-items: center;
+        margin-left: 20px;
+    }
+
+    .icono {
+        width: 100px;
+        height: 80px;
+        margin-right: 10px;
+    }
+
+    .rounded-icono {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+
+    .small-icono {
+        width: 40px;
+        height: 40px;
+        margin-bottom: 35px;
+    }
+
+    .text-container {
+        text-align: left;
+        color: white;
+    }
+
+    .logo-text {
+        font-weight: bold;
+        font-size: 40px;
+        margin-left: 10px;
+    }
+
+    .subtitle {
+        font-size: 20px;
+        margin-left: 40px;
+        margin-bottom: 10px;
+    }
+
+    .spacer {
+        flex: 1;
+    }
+
+    .button-container {
+        color: #ffffff;
+        font-size: 18px;
+        text-align: right;
+        margin-right: 20px;
+        display: flex;
+        align-items: center;
+    }
+
+    .boton {
+        color: #ffffff;
+        background-color: transparent;
+        border-color: transparent;
+    }
+
+    .vertical-line {
+        width: 1px;
+        height: 40px;
+        background-color: white;
+        margin-left: 10px;
+        margin-right: 20px;
+    }
+
+</style>
