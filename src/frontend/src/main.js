199f5ae471220315477460f564238b2f22a891ef
rafa <rafajeremias@live.com>
2023-11-13 02:22:07 -0300

    Interfaz de Registro
    se ha creado la interfaz de registro correspondiente al mockup y dejando listo para ser usado en mongo

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -3,6 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
 
 // Para cambiar el tema de PrimeVue, elegir otro import de: https://primevue.org/theming/
 import 'primevue/resources/themes/lara-light-blue/theme.css'
@@ -18,12 +24,19 @@ import ToastService from 'primevue/toastservice';
 // importación de rutas
 import router from './router';
 
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    
+  })
 const app = createApp(App)
 
 //Uso de dependencias
 app.use(createPinia())
 app.use(PrimeVue);
 app.use(ToastService);
-app.use(router);
+app.use(router); 
+app.use(vuetify);
 
 app.mount('#app')
--- src/frontend/src/router.js
+++ src/frontend/src/router.js
@@ -6,16 +6,26 @@ const routes = [
     name: 'index',
     component: () => import('./pages/index.vue'),
   },
+
   {
     path: '/home',
     name: 'home',
     component: () => import('./pages/home.vue'),
   },
+
+  {
+    path: '/registerpage',
+    name: 'registerpage',
+    component: () => import('./pages/register.vue')
+  }
+
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+
 
 export default router;
