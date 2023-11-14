0ae29c1178a77f183734b33bd52141106d6d7ae3
Matias Marquez San Martin <marquez-514@hotmail.com>
2023-11-14 17:44:13 -0300

    Se arreglo la conexión con la base de datos y se dejo un mensaje de confirmación

--- src/frontend/src/components/Register/RegisterPage.vue
+++ src/frontend/src/components/Register/RegisterPage.vue
@@ -2,7 +2,8 @@
   <div class="registro-container">
     <div class="image-container">
       <h1 style="color: #ee451b">Bancos<br>Tusunami</h1>
-      <img src="https://trello.com/1/cards/654d83f9682db5e91c4b4d00/attachments/654ff6e5f6f7c785ecf36ba5/previews/654ff6e6f6f7c785ecf36bbd/download/pngwing.com.png"
+      <img
+        src="https://trello.com/1/cards/654d83f9682db5e91c4b4d00/attachments/654ff6e5f6f7c785ecf36ba5/previews/654ff6e6f6f7c785ecf36bbd/download/pngwing.com.png"
         alt="Imagen de fondo" width="510" height="510" />
     </div>
     <div class="form-container">
@@ -13,7 +14,8 @@
       <form @submit.prevent="login">
         <div class="input-container">
           <label style="color:#103ed4" for="username">Nombre Completo:</label>
-          <input type="text" id="username" :maxlength="50" :counter="50" @keypress="soloLetras" v-model="completeName" required>
+          <input type="text" id="username" :maxlength="50" :counter="50" @keypress="soloLetras" v-model="completeName"
+            required>
         </div>
         <div class="input-container">
           <label style="color:#103ed4" for="rut">Rut:</label>
@@ -23,7 +25,8 @@
         </div>
         <div class="input-container">
           <label style="color:#103ed4" for="text">Correo electrónico:</label>
-          <input @input="validarEmail" type="text" id="correo electronico" :maxlength="50" :counter="50" v-model="email" required>
+          <input @input="validarEmail" type="text" id="correo electronico" :maxlength="50" :counter="50" v-model="email"
+            required>
           <p v-if="esValido"></p>
           <p v-else style="color: black">El correo electrónico debe contener al menos un "@".</p>
         </div>
@@ -32,9 +35,10 @@
           <input type="password" id="password" :maxlength="50" :counter="50" v-model="password" required>
         </div>
 
-        <v-select style="color: #103ed4;" v-model="sucursal" label="Sucursal" :items="[1, 2, 3]" variant="outlined" required></v-select>
+        <v-select style="color: #103ed4;" v-model="sucursal" label="Sucursal" :items="[1, 2, 3]" variant="outlined"
+          required></v-select>
 
-        <v-btn block :color="esValido ? '#ee451b' : 'grey'" type="submit" @click="verificarYCrearUsuario"
+        <v-btn block :color="esValido ? '#ee451b' : 'grey'" type="submit" @click="crearUsuario"
           :disabled="!(completeName.length > 0 && password.length > 0 && esRutValido)">Registrar</v-btn>
         <div>
           <h3 style="color:#103ed4">¿Ya tienes cuenta?
@@ -65,21 +69,28 @@ export default {
   },
 
   methods: {
-    async verificarYCrearUsuario() {
-      if (this.validarRut() && this.validarEmail()) {
-        await this.crearUsuario();
-      } else {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error de Registro',
-          text: 'Rut o correo incorrecto',
-        });
-        console.log("Error de registro");
-      }
-    },
 
     async crearUsuario() {
-      // ... (código anterior) ...
+      const nombre = this.completeName.split(' ');
+      const numeroDeUsuarios = await API.getNumeroUsuarios();
+      await API.addUsuario({
+        "nombres": nombre[0] + " " + nombre[1],
+        "apellidoPaterno": nombre[2],
+        "apellidoMaterno": nombre[3],
+        "email": this.email.toLowerCase(),
+        "rut": this.rut,
+        "password": this.password,
+        "sucursal": this.sucursal,
+        "idUsuario": numeroDeUsuarios + 1
+      })
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Se creo el usuario con exito',
+        text: '',
+      }
+      )
+      console.log('Creando usuario...');
     },
 
     validarEmail() {
@@ -117,88 +128,88 @@ export default {
 </script>
 
 <style setup>
-  h1 {
-    font-size: 50px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-weight: bolder;
-    text-align: center;
-  }
+h1 {
+  font-size: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bolder;
+  text-align: center;
+}
 
-  .registro-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-  }
+.registro-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
 
-  .image-container {
-    flex: 1;
-    padding: 50px;
-  }
+.image-container {
+  flex: 1;
+  padding: 50px;
+}
 
-  .image-container img {}
+.image-container img {}
 
-  .form-container {
-    flex: 1;
-    padding-left: 150px;
-    padding-right: 150px;
-    padding-bottom: 30px;
-    padding-top: 10px;
-    background-color: #ffffff;
-    border-radius: 5px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  }
+.form-container {
+  flex: 1;
+  padding-left: 150px;
+  padding-right: 150px;
+  padding-bottom: 30px;
+  padding-top: 10px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+}
 
-  h2 {
-    margin-top: 0;
-    font-weight: bolder;
-    align-items: center;
-    justify-content: center;
-    font-weight: bolder;
-    text-align: center;
-  }
+h2 {
+  margin-top: 0;
+  font-weight: bolder;
+  align-items: center;
+  justify-content: center;
+  font-weight: bolder;
+  text-align: center;
+}
 
-  .input-container {
-    margin-bottom: 15px;
-    text-align: left;
-    text-decoration: underline;
-    text-decoration-color: #103ed4;
-  }
+.input-container {
+  margin-bottom: 15px;
+  text-align: left;
+  text-decoration: underline;
+  text-decoration-color: #103ed4;
+}
 
-  label {
-    display: block;
-    font-weight: bold;
-  }
+label {
+  display: block;
+  font-weight: bold;
+}
 
-  input[type="text"],
-  input[type="password"] {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #0d60c0;
-    border-radius: 5px;
-  }
+input[type="text"],
+input[type="password"] {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #0d60c0;
+  border-radius: 5px;
+}
 
-  button {
-    background-color: #ee451b;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    text-align: center;
-  }
+button {
+  background-color: #ee451b;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  text-align: center;
+}
 
-  button:hover {
-    background-color: #ee451b;
-  }
+button:hover {
+  background-color: #ee451b;
+}
 
-  .redirigir {
-    display: inline;
-  }
+.redirigir {
+  display: inline;
+}
 
-  body {
-    background-color: #c7c7c7;
-  }
-</style>
+body {
+  background-color: #c7c7c7;
+}
+</style> 
