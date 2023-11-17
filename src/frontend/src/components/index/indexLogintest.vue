8375be60815d1103d14bc5389e64a9ad2f54572e
maxxandres <80993475+maxxandres@users.noreply.github.com>
2023-11-17 01:25:40 -0300

    Update indexLogintest.vue

--- src/frontend/src/components/index/indexLogintest.vue
+++ src/frontend/src/components/index/indexLogintest.vue
@@ -10,20 +10,6 @@
                     Hola aqui va el Login, hay que eliminar toda esta pagina (indexLogintest) y cambiarla por la otra
                   </v-card-title>
                   <v-card-text>
-                    <v-row v-if="botonActivado" justify="center">
-                      <v-col>
-                        <v-img src="https://sm.ign.com/ign_es/news/t/the-exorci/the-exorcist-reportedly-getting-a-2021-reboot_ybaj.jpg" :width="800" :height="800"></v-img>
-                      
-                    
-                        <v-img src="https://ih1.redbubble.net/image.5166749047.4146/pp,504x498-pad,600x600,f8f8f8.jpg" :width="800" :height="800"></v-img>
-                      </v-col>
-                    </v-row>
-                    <!-- Se muestra el botón que cambia entre dos imágenes -->
-                    <v-btn @click="mostrarImagenes" color="primary">
-                      No pulses este boton 
-                    </v-btn>
-                    <!-- Se muestran las dos imágenes cuando el botón está activado -->
-                    
                   </v-card-text>
                 </v-card>
               </v-col>
@@ -38,15 +24,11 @@
   export default {
     data() {
       return {
- 
-        botonActivado: false
+
       };
     },
     methods: {
-      mostrarImagenes() {
-        // Activa o desactiva el botón para mostrar las imágenes
-        this.botonActivado = !this.botonActivado;
-      }
+
     }
   };
   </script>
