f33f3c255f4ce5f6aec58db3bbf55bcc4ae7a90d
Nicolas Alejandro Lopez Paredes <nilopez19@alumnos.utalca.cl>
2023-11-18 20:46:50 -0300

    se crea el modulo para crear la cuenta corriente
    crear el page de "creacion_cuenta" en la carpeta src  (Aqui se debe llamar al formulario)y crear el componente de crearCuentaCorriente en la carpeta components/creacionCuenta/(Aqui debe ir el formulario)

---
+++ src/frontend/src/components/creacionCuentas/ctaCorriente.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>porque no se ve estoooooo</h1>
+  <div>
+    <v-btn> Button </v-btn>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ src/frontend/src/pages/creacioncuentas.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ctaCorriente from '../components/creacionCuentas/ctaCorriente.vue';
+</script>
+
+
+<template>
+    <ctaCorriente/>
+</template>
+
+<style scoped>
+</style>
