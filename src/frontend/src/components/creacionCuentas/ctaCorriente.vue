3e9513776973b47ab7ed280485a801b5632e2a8c
Nicolas Alejandro Lopez Paredes <nilopez19@alumnos.utalca.cl>
2023-11-18 21:33:28 -0300

    estructura de cuenta corriente

--- src/frontend/src/components/creacionCuentas/ctaCorriente.vue
+++ src/frontend/src/components/creacionCuentas/ctaCorriente.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="titulo">
-    <h1>Cuenta de ahorro</h1>
-  </div>
-  <div></div>
+    <div>
+      <h1 class="titulo">Cuenta de ahorro</h1>
+    </div>
+  
 </template>
 
 <script></script>
 
 <style>
+body{
+    background-color: white;
+}
 .titulo{
   text-align: center;
   padding: 2rem;
-  color: white;
+  color: black;
 }
 </style>
